54635b33dbe2ee35e4a350df3330108c08008aa1
noname-rt1d34 <fujimi-bentley-macquarie@hotmail.com>
2026-02-15 14:17:35 +1100

    test3

--- libs/utilities.js
+++ libs/utilities.js
@@ -20,7 +20,7 @@ Want to create a method for handling data conversion from different bases
 //Gives back a string, expects an array as input
 function JIMS_B256arraytoHex(datain, datalen, uppercase=true){
     let output = "";
-    if (uppercase === true){
+    if (uppercase == true){
         const lookup = ['0','1', '2', '3', '4', '5', '6','7','8','9','A','B','C','D','E','F'];
     } else {
         const lookup = ['0','1', '2', '3', '4', '5', '6','7','8','9','a','b','c','d','e','f'];
